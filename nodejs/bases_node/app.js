aa9c57743b4d50f903ff0398d363ab40e5022719
josmig <pantigosomiguel59@gmail.com>
2021-08-19 23:08:56 -0500

    utilizando Yargs

--- nodejs/bases_node/app.js
+++ nodejs/bases_node/app.js
@@ -1,15 +1,39 @@
 const { crearArchivo } = require('./helpers/multiplicar')
+const yargs = require('yargs').argv;
+
 console.clear();
+/* const base = 4; */
+
+console.log(process.argv );
+console.log( yargs );
+
+
+console.log('base : yargs', yargs.base);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /* const base = 8; */
-
+/* 
 const [,,arg3  = "base=5"] = process.argv;
 const [, base = '5'] = arg3.split('=');
-
+ */
 /* console.log(arg3); */
-console.log(base);
+/* console.log(base);
+ */
 
-
-crearArchivo(base)
+/* crearArchivo(base)
     .then(nameArchivo => console.log(nameArchivo, 'creado'))
-    .catch(err =>  console.log(err));
+    .catch(err =>  console.log(err)); */
